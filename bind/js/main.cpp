328e5bf0c0e1b4c143e0bf8a734f903d4e4009f0
cybermeng <cybermeng@hotmail.com>
2025-07-10 19:21:49 +0800

    clean

--- bind/js/main.cpp
+++ bind/js/main.cpp
@@ -88,7 +88,7 @@ const char* run_pine_calculation(const char* bytecode_string_c, const char* fina
             }
             
             if (num_bars > 0) {
-                std::cout << "\nVM start" << num_bars << std::endl;
+                std::cout << "\nVM start, bar number:" << num_bars << std::endl;
                 PineVM vm(num_bars);
                 parse_and_load_data(vm, financial_data_string);
                 vm.loadBytecode(bytecode_string);
